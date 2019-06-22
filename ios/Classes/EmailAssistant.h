fb43e008de952d33c15b32938ea4db17fe20cde3
wang animeng <animeng68@gmail.com>
2019-06-22 11:36:58 +0800

    add email , share,rate function

---
+++ ios/Classes/EmailAssistant.h
@@ -0,0 +1,16 @@
+//
+//  EmailAssistant.h
+//  device_info
+//
+//  Created by wang animeng on 2019/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EmailAssistant : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Classes/EmailAssistant.m
@@ -0,0 +1,12 @@
+//
+//  EmailAssistant.m
+//  device_info
+//
+//  Created by wang animeng on 2019/6/22.
+//
+
+#import "EmailAssistant.h"
+
+@implementation EmailAssistant
+
+@end
